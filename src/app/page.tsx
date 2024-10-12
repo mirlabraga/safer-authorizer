e00fbf55fc5436e37e813b85925dfdd9d9e34632
Mirla Braga <mirla.chucre@dft.gov.uk>
2024-10-12 07:25:53 +0100

    refactoring the Page js for callback function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,23 @@ import React from "react";
 import { Auth0Provider } from "@auth0/auth0-react";
 import Profile from "./profile";
 
+
 export default function MyApp() {
+
+  let CallbackURI = "";
+
+  if (typeof window !== "undefined" && window.location !== null) {
+    CallbackURI = window.location.origin;
+  }
+
   return (
     <div>
       <Auth0Provider
         domain="http://localhost:3000"
         clientId="gS3C20Vt9iT7qy2yprLHjw7SuK8IDWjQ"
-        onRedirectCallback={() => "http://localhost:3000"}
+        authorizationParams={{
+          redirect_uri: CallbackURI
+        }}
       >
         <Profile />
       </Auth0Provider>
