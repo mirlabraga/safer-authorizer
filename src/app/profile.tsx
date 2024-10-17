c3d4c32edb1abb68ea944a76e65958e89766b36d
Mirla Braga <mirla.chucre@dft.gov.uk>
2024-10-17 11:57:30 +0100

    log out and start profile user with auth0

--- src/app/profile.tsx
+++ src/app/profile.tsx
@@ -2,7 +2,8 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
 const Profile = () => {
-  const { isAuthenticated, loginWithRedirect, isLoading } = useAuth0();
+  const { user, isAuthenticated, loginWithRedirect, isLoading, logout } =
+    useAuth0();
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -11,9 +12,13 @@ const Profile = () => {
   if (isAuthenticated) {
     return (
       <div>
-        Hello {}{' '}
-        <button onClick={() => console.log("fadsf")}>
-          Log out
+        Hello {user?.name}{" "}
+        <button
+          onClick={() =>
+            logout({ logoutParams: { returnTo: window.location.origin } })
+          }
+        >
+          Log Out
         </button>
       </div>
     );
